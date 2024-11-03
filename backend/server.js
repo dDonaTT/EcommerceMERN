87c5e176ac25472e605f46c4fa28806cf8e203db
DonaTT <116920667+dDonaTT@users.noreply.github.com>
2024-11-03 23:22:53 +0100

    auth added and backed configurated

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import productRoutes from "./routes/product.routes.js";
+import cartRoutes from "./routes/cart.routes.js";
+import couponRoutes from "./routes/coupon.routes.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server runing on port ${PORT}`);
+  connectDB();
+});
